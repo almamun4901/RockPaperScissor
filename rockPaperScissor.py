fd723b0ea0ab32a30b2701a2e725b9ce1fed2acf
AlMamun <almamun4901@gmail.com>
2022-10-24 01:38:17 -0400

    adding comment

--- rockPaperScissor.py
+++ rockPaperScissor.py
@@ -1,23 +1,51 @@
+#Author: MD AL MAMUN
+#Date: 10/24/2022
+# ROCK PAPER SCISSOR GAME
+
+
 import random
-from random import randint
+
 def game():
     play = True
+    userPoint = 0
+    computerPoint = 0
+
+    #computer inputs
     computer = ['r','p','s']
+
+    #setting infinite loop
     while play:
+
+        #taking user input
         userInput = input("Enter your r or p or s : ")
+
+        #genrating computer input
         x = random.randint(0,2)
         computerInput = computer[x]
+
+        #setting game quit
         if userInput == 'q':
             play = False
             print("GAME QUIT")
             break
+
+        #checking draw
         elif userInput == computerInput:
             print("draw")
+            print('User:', userPoint)
+            print("Computer", computerPoint)
 
+        #checking user win
         elif (userInput == 's' and computerInput == 'p') or (userInput == 'p' and computerInput == 'r') or (userInput == 'r' and computerInput == 's'):
             print('User Win')
+            userPoint = userPoint + 1
+            print('User:', userPoint)
+            print("Computer", computerPoint)
 
+        #checking computer win
         else:
             print("computer win")
-
+            computerPoint = computerPoint + 1
+            print('User:', userPoint)
+            print("Computer", computerPoint)
 game()
